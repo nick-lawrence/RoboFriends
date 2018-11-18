5410b6828f841370455320b35aa44886ba907a8d
nick-lawrence <nicklawrence.design@gmail.com>
2018-11-18 12:21:59 -0800

    changed the font for index and added search feature.

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots'; //<---- Not default export so must be destructured
+import './App.css';
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={ robots }/>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+
+  }
+  render() {
+      const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className='tc'>
+        <h1 class="title">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={ filteredRobots }/>
+      </div>
+    );
+  }
 }
+ 
+
 
 export default App;
