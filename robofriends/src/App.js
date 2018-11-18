b5a745691db28f25b98641d128ad273136b03480
nick-lawrence <nicklawrence.design@gmail.com>
2018-11-18 13:15:28 -0800

    Added scroll on body and pulling from JSON API

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots'; //<---- Not default export so must be destructured
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
   }
 
   onSearchChange = (event) => {
@@ -20,14 +26,19 @@ class App extends Component {
   render() {
       const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    });
-    return (
-      <div className='tc'>
-        <h1 class="title">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={ filteredRobots }/>
-      </div>
-    );
+    })
+    if (this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className="title">RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={ filteredRobots }/>
+          </Scroll>
+        </div>
+    )}
   }
 }
  
